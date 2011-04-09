5dcd2d78dbd9086f7309f0395aafba768963734c
mnichols <ceiapps@gmail.com>
2011-04-09 15:20:37 -0700

    a few code docs

--- src/app/Ditto/Internal/IResolutionContext.cs
+++ src/app/Ditto/Internal/IResolutionContext.cs
@@ -3,15 +3,48 @@
 namespace Ditto.Internal
 {
 
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IResolutionContext
     {
+        /// <summary>
+        /// Gets the type of the source for current resolution.
+        /// </summary>
+        /// <value>The type of the source.</value>
         Type SourceType { get; }
+        /// <summary>
+        /// Gets the source object. Note this the whole source object, not just its property value.
+        /// </summary>
+        /// <value>The source.</value>
         object Source { get; }
+        /// <summary>
+        /// Gets the destination. This is the declaring object for the resulting value, not the destination property itself.
+        /// </summary>
+        /// <value>The destination.</value>
         object Destination { get; }
+
+        /// <summary>
+        /// Used by <c>ditto </c> to prepare the destination property for value assignment during execution.
+        /// </summary>
+        /// <param name="destinationProperty">The destination property.</param>
+        /// <returns></returns>
         IAssignValue BuildValueAssignment(IDescribeMappableProperty destinationProperty);
         object GetSourcePropertyValue(string propertyName);
         object GetDestinationPropertyValue(string propertyName);
+        /// <summary>
+        /// Nests this context only on the provided <paramref name="destinationProperty"/>. The source declaring object remains the same in the resulting context.
+        /// </summary>
+        /// <param name="destinationProperty">The destination property.</param>
+        /// <returns></returns>
         IResolutionContext Nested(IDescribeMappableProperty destinationProperty);
+
+        /// <summary>
+        /// Nests this context on both the destination and source properties. 
+        /// </summary>
+        /// <param name="destinationProperty">The destination property.</param>
+        /// <param name="sourceProperty">The source property.</param>
+        /// <returns></returns>
         IResolutionContext Nested(IDescribeMappableProperty destinationProperty, IDescribeMappableProperty sourceProperty);
     }
 
